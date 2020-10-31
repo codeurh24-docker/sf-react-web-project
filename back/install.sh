17eca0d109da2c23bd42f006418cb8937af7a051
codeurh24 <cci.corlouer@gmail.com>
2020-10-31 17:42:39 +0100

    - create database - create user in db

--- back/install.sh
+++ back/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# apt install php-curl php-gd php-intl php-json php-mbstring php-xml php-zip
-# apt-get install -y php8.0-common php8.0-fpm php8.0 php8.0-zip php8.0-soap php8.0-opcache php8.0-mysql php8.0-cli php8.0-gd php8.0-curl php8.0-xsl php8.0-imap php8.0-intl php8.0-sqlite3 php8.0-bcmath php8.0-mbstring php8.0-readline
+echo "START install.sh"
 cd /var/www/html
 chmod -R 777 /var/www/html
+echo "COMPOSER INSTALL"
 composer install
 chmod -R 777 /var/www/html
 rm -r /var/cache/*
 rm -r /var/log/*
+echo "CREATE DATABASE"
+cd /var/www/html
+php bin/console d:d:c
+php bin/console d:s:u --force
 echo "RUN SERVER BACK"
 cd /var/www/html
 php -S 0.0.0.0:8000 -t public
